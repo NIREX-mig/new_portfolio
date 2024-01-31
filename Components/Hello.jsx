e5dbca6cd28f67b4a32fc00d56f1f3f2d4a7f43b
Nirex <akay93796@gmail.com>
2024-01-31 23:58:31 +0530

    fix:hello section image

--- Components/Hello.jsx
+++ Components/Hello.jsx
@@ -8,7 +8,7 @@ const Hello = () => {
       <h1 className="capitalize lg:text-5xl text-2xl text-gray-900 font-bold font-[EuroStyle Normal] pb-8 ">frontend devloper & MERN stack devloper</h1>
       <p className="lg:text-2xl text-lg capitalize">I design and code beautifully simple things, and I love what I do.</p>
 
-      <Image src="/assets/nirex-avatar.svg" alt="svg" width={250} height={250} className="mx-auto lg:pt-24 p-12"/>
+      <Image src="/assets/nirex-avatar.svg" alt="svg" width={250} height={250} className="mx-auto lg:pt-24 p-12 lg:p-0"/>
       </div>
     </section>
   )
