52304d61df1fee70be46925cbc511f3a91fdfb4c
Nirex <akay93796@gmail.com>
2024-02-04 13:51:50 +0530

    add: top loading bar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 const poppins = Poppins({
   weight: "500",
@@ -17,11 +18,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-      <html lang="en">
-        <body className={poppins.className}>
-          {children}
-          <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
-        </body>
-      </html>
+    <html lang="en">
+      <body className={poppins.className}>
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
+        {children}
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+      </body>
+    </html>
   );
 }
