b0e27936a7bd0ab322d909444310b4d4c9cd7de7
nirex <akay93796@gmail.com>
2024-05-24 07:19:36 +0530

    refactor UI

--- Components/ResumeCard.jsx
+++ Components/ResumeCard.jsx
@@ -3,9 +3,9 @@ const ResumeCard = (props) => {
     const {session, company, address} = props;
   return (
     <article className="px-10 py-3 rounded-md my-2">
-        <p className="text-sm ">{session}</p>
-        <h3 className="text-lg  my-1">{company}</h3>
-        <p className="text-sm">{address}</p>
+        <p className="text-xs ">{session}</p>
+        <h3 className="text-lg font-medium my-1">{company}</h3>
+        <p className="text-xs">{address}</p>
     </article>
   )
 }
